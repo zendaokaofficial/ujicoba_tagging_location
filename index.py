9366e2aabc8dad653eb6a47e61b127101766f4b7
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-11-07 18:42:46 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,3 @@
-# app.py
-
 import streamlit as st
 import geocoder
 from shapely.geometry import Point, MultiPoint, MultiPolygon
@@ -7,24 +5,23 @@ import pandas as pd
 
 st.title("Tag Multiple GPS Locations to Create a MultiPolygon")
 
-# Initialize session state to store tagged locations
-if "tagged_locations" not in st.session_state:
-    st.session_state.tagged_locations = []
-
 # Sidebar input for tagging
 st.sidebar.markdown("Tag your GPS locations:")
+
+# Initialize a list to store the tagged locations
+tagged_locations = []
 
 # Button to get the current location
 if st.sidebar.button("Get Current Location"):
     location = geocoder.ip("me")
 
     if location:
-        latitude = location.latlng[0]
-        longitude = location.latlng[1]
+        latitude = round(location.latlng[0], 7)  # Round latitude to 7 decimal places
+        longitude = round(location.latlng[1], 7)  # Round longitude to 7 decimal places
 
         # Store the tagged location as a Point
         point = Point(longitude, latitude)
-        st.session_state.tagged_locations.append(point)
+        tagged_locations.append(point)
 
         st.success(f"Location tagged at Latitude: {latitude}, Longitude: {longitude}")
     else:
@@ -33,24 +30,23 @@ if st.sidebar.button("Get Current Location"):
 # Display tagged locations
 st.sidebar.title("Tagged Locations")
 
-if st.session_state.tagged_locations:
+if tagged_locations:
     st.sidebar.markdown("Tagged Locations:")
     location_table = pd.DataFrame(
         {
-            "Latitude": [point.y for point in st.session_state.tagged_locations],
-            "Longitude": [point.x for point in st.session_state.tagged_locations],
+            "Latitude": [f"{point.y:.7f}" for point in tagged_locations],  # Format to 7 decimal places
+            "Longitude": [f"{point.x:.7f}" for point in tagged_locations],  # Format to 7 decimal places
         }
     )
     st.sidebar.dataframe(location_table)
 
 # Button to create a MultiPolygon
 if st.sidebar.button("Create MultiPolygon"):
-    if st.session_state.tagged_locations:
-        multi_point = MultiPoint(st.session_state.tagged_locations)
+    if tagged_locations:
+        multi_point = MultiPoint(tagged_locations)
         multi_polygon = multi_point.convex_hull
 
-        # Store the MultiPolygon in session state
-        st.session_state.multi_polygon = multi_polygon
+        # You can save the MultiPolygon to a file or perform other actions here.
         st.success("MultiPolygon created.")
 
 # Display the MultiPolygon
